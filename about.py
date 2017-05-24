7c571c98b3ad6ca4c448af8cea907cf283699745
Your Name <you@example.com>
2017-05-24 10:17:28 -0600

    [fixed] Project Description

--- about.py
+++ about.py
@@ -11,7 +11,7 @@ def display_about_view():
 
     about_desc = Text(about_view, width= 65, height = 8, bg = "#CCC")
     about_desc.pack()
-    about_desc.insert(END, "GitHub es una forja (plataforma de desarrollo colaborativo) para alojar proyectos utilizando el sistema de control de versiones Git. Utiliza el framework Ruby on Rails por GitHub, Inc. (anteriormente conocida como Logical Awesome). Desde enero de 2010, GitHub opera bajo el nombre de GitHub, Inc. El código se almacena de forma pública, aunque también se puede hacer de forma privada, creando una cuenta de pago.")
+    about_desc.insert(END, "GNU/Linux, es el término empleado para referirse a la combinación del sistema operativo GNU, desarrollado por la FSF, y el núcleo(kernel) Linux, desarrollado por Linus Torvalds y la Linux Foundation. Su desarrollo es uno de los ejemplos más prominentes de software libre; todo su código fuente puede ser utilizado, modificado y redistribuido libremente por cualquiera bajo los términos de la GPL (Licencia Pública General de GNU) y otra serie de licencias libres")
     about_desc.config(state=DISABLED)
     about_desc.place(x = 20, y = 60)
      
